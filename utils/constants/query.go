582a3aecd1c5a0f4a6e02f166e6d0e1aefd3a650
Jefri Agistar <108538799+0jefri@users.noreply.github.com>
2024-11-05 10:38:38 +0700

    Update query.go

--- utils/constants/query.go
+++ utils/constants/query.go
@@ -6,6 +6,4 @@ const (
 	WHERE_BY_PRODUCT_ID = "id = ?"
 	ReceiveProductType  = "receiveProduct"
 	SendProductType     = "sendProduct"
-	// WHERE_BY_BILL_ID             = "bill_id = ?"
-	// WHERE_BY_USER_ID_AND_CARD_ID = "user_id = ? AND id = ?"
 )
